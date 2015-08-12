bb9db1a1efd0d5d19e6cb45a1dd074cea1d623a3
Warren Rexroad <wrexroad@gmail.com>
2015-08-12 23:44:24 +0000

    fixed inequlity sign for hkpg rebin. added rcnt rebin

--- soc-package/home/barrel/nodeServices/rebin.js
+++ soc-package/home/barrel/nodeServices/rebin.js
@@ -89,10 +89,10 @@ var rebinner = {
          //check for min and max in all hkpg values
          for (var_i in hkpg) {
             if (+hkpg[var_i] || hkpg[var_i] === 0) {
-               if (!(loVal[var_i] > hkpg[var_i])) {
+               if (!(loVal[var_i] < hkpg[var_i])) {
                   loVal[var_i] = hkpg[var_i];
                }
-               if (!(hiVal[var_i] < hkpg[var_i])) {
+               if (!(hiVal[var_i] > hkpg[var_i])) {
                   hiVal[var_i] = hkpg[var_i];
                }
             }
@@ -109,7 +109,6 @@ var rebinner = {
          _id: thisBinId + (binWidth / 2), hkpg: {}
       };
       for (var_i in hkpg) {
-         //save the min and the max as two neighboring points
          rebinned[bin_i].hkpg[var_i] = +loVal[var_i];
          rebinned[bin_i + 1].hkpg[var_i] = +hiVal[var_i];
       }
@@ -117,10 +116,104 @@ var rebinner = {
       return rebinned;
    },
    rcnt : function(payload, docs, binLvL) {
-      var rebinned = [];
-      if(binLvL < 3) {
+      var
+         numDocs = docs.length,
+         binWidth = Math.pow(2, binLvL),
+         loVal = {},
+         hiVal = {},
+         rebinned = [],
+         doc_i, bin_i, thisBinId, lastBinId;
+
+      if (binLvL < 3) {
+         //binning level too low, there would be less than 1 record per bin
          return docs;
       }
+      if (!numDocs) {
+         return null;
+      }
+      
+      //set the first value of 'binId' and start an empty document.
+      thisBinId = lastBinId = docs[0]._id - (docs[0]._id % binWidth); 
+
+      for (doc_i = 0, bin_i = 0; doc_i < numDocs; doc_i++) {
+         thisBinId = docs[doc_i]._id - (docs[doc_i]._id % binWidth);
+         if (thisBinId != lastBinId) {
+            rebinned[bin_i] = {
+               _id: thisBinId, rcnt: {}
+            };
+            rebinned[bin_i + 1] = {
+               _id: thisBinId + (binWidth / 2), rcnt: {}
+            };
+            rebinned[bin_i].rcnt = {
+               '0' : +loVal['0'],
+               '1' : +loVal['1'],
+               '2' : +loVal['2'],
+               '3' : +loVal['3']
+            };
+            rebinned[bin_i + 1].rcnt = {
+               '0' : +hiVal['0'],
+               '1' : +hiVal['1'],
+               '2' : +hiVal['2'],
+               '3' : +hiVal['3']
+            };
+
+            loVal = {};
+            hiVal = {};
+            bin_i += 2;
+         }
+         
+         if (!(+rcnt[var_i] || rcnt[var_i] === 0)) {
+            //invalid data point
+            continue;
+         }
+
+         if (!(loVal['0'] < hkpg['0'])) {
+            loVal['0'] = hkpg['0'];
+         };
+         if (!(loVal['1'] < hkpg['1'])) {
+            loVal['1'] = hkpg['1'];
+         };
+         if (!(loVal['2'] < hkpg['2'])) {
+            loVal['2'] = hkpg['2'];
+         };
+         if (!(loVal['3'] < hkpg['3'])) {
+            loVal['3'] = hkpg['3'];
+         };
+         if (!(hiVal['0'] > hkpg['0'])) {
+            hiVal['0'] = hkpg['0'];
+         };
+         if (!(hiVal['1'] > hkpg['1'])) {
+            hiVal['1'] = hkpg['1'];
+         };
+         if (!(hiVal['2'] > hkpg['2'])) {
+            hiVal['2'] = hkpg['2'];
+         };
+         if (!(hiVal['3'] > hkpg['3'])) {
+            hiVal['3'] = hkpg['3'];
+         };
+      
+         lastBinId = thisBinId;
+      }
+      
+      rebinned[bin_i] = {
+         _id: thisBinId, rcnt: {}
+      };
+      rebinned[bin_i + 1] = {
+         _id: thisBinId + (binWidth / 2), rcnt: {}
+      };
+      rebinned[bin_i].rcnt = {
+         '0' : +loVal['0'],
+         '1' : +loVal['1'],
+         '2' : +loVal['2'],
+         '3' : +loVal['3']
+      };
+      rebinned[bin_i + 1].rcnt = {
+         '0' : +hiVal['0'],
+         '1' : +hiVal['1'],
+         '2' : +hiVal['2'],
+         '3' : +hiVal['3']
+      };
+
       return rebinned;
    },
    ephm : function(payload, docs, binLvL) {
